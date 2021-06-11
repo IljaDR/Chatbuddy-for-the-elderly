b007d9130301dec898b8163428c5773b301b6e95
IljaDR <38908439+IljaDR@users.noreply.github.com>
2021-06-11 21:00:02 +0200

    Added unity interface

---
+++ Unity interface/Assets/Scripts/Client.cs
@@ -0,0 +1,151 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Net;
+using System.Net.Sockets;
+using System;
+using System.Text;
+
+// Code from https://github.com/tom-weiland/tcp-udp-networking
+public class Client : MonoBehaviour
+{
+    public static Client instance;
+    public static int dataBufferSize = 1024;
+
+    public string ip = "127.0.0.1";
+    public int port = 10000;
+    public int myId = 0;
+    public TCP tcp;
+    public FishManager fish;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Debug.Log("Instance already exists, destroying object!");
+            Destroy(this);
+        }
+    }
+
+    private void Start()
+    {
+        tcp = new TCP(fish);
+        ConnectToServer();
+    }
+
+    public void ConnectToServer()
+    {
+        tcp.Connect();
+    }
+
+    public class TCP
+    {
+        public TcpClient socket;
+
+        private NetworkStream stream;
+        private Packet receivedData;
+        private byte[] receiveBuffer;
+        private FishManager fish;
+
+        public TCP(FishManager fish)
+        {
+            this.fish = fish;
+        }
+
+        public void Connect()
+        {
+            socket = new TcpClient
+            {
+                ReceiveBufferSize = dataBufferSize,
+                SendBufferSize = dataBufferSize
+            };
+
+            receiveBuffer = new byte[dataBufferSize];
+            socket.BeginConnect(instance.ip, instance.port, ConnectCallback, socket);
+        }
+
+        private void ConnectCallback(IAsyncResult _result)
+        {
+            socket.EndConnect(_result);
+
+            if (!socket.Connected)
+            {
+                return;
+            }
+
+            stream = socket.GetStream();
+
+            receivedData = new Packet();
+
+            stream.BeginRead(receiveBuffer, 0, dataBufferSize, ReceiveCallback, null);
+        }
+
+        public void SendData(Packet _packet)
+        {
+            try
+            {
+                if (socket != null)
+                {
+                    stream.BeginWrite(_packet.ToArray(), 0, _packet.Length(), null, null);
+                }
+            }
+            catch (Exception _ex)
+            {
+                Debug.Log($"Error sending data to server via TCP: {_ex}");
+            }
+        }
+
+        private void ReceiveCallback(IAsyncResult _result)
+        {
+
+            try
+            {
+                int _byteLength = stream.EndRead(_result);
+                if (_byteLength <= 0)
+                {
+                    // TODO: disconnect
+                    return;
+                }
+
+                byte[] _data = new byte[_byteLength];
+                Array.Copy(receiveBuffer, _data, _byteLength);
+
+                string emotion = Encoding.ASCII.GetString(receiveBuffer, 0, _byteLength);
+
+                switch (emotion)
+                {
+                    case "anger":
+                        fish.fishEmotion = FishManager.Emotion.anger;
+                        break;
+                    case "fear":
+                        fish.fishEmotion = FishManager.Emotion.fear;
+                        break;
+                    case "sadness":
+                        fish.fishEmotion = FishManager.Emotion.sadness;
+                        break;
+                    case "joy":
+                        fish.fishEmotion = FishManager.Emotion.joy;
+                        break;
+                    case "neutral":
+                        fish.fishEmotion = FishManager.Emotion.neutral;
+                        break;
+                    default:
+                        Debug.Log("Failed to assign emotion");
+                        break;
+                }
+                fish.emotionCountdown = 7;
+                fish.countdownLength = 15;
+                /*receivedData.Reset(HandleData(_data));*/
+                stream.BeginRead(receiveBuffer, 0, dataBufferSize, ReceiveCallback, null);
+            }
+            catch
+            {
+                // TODO: disconnect
+            }
+        }
+    }
+}
---
+++ Unity interface/Assets/Scripts/FishManager.cs
@@ -0,0 +1,197 @@
+﻿using UnityEngine;
+
+public class FishManager : MonoBehaviour
+{
+    public enum Emotion
+    {
+        anger,
+        fear,
+        sadness,
+        joy,
+        neutral
+    }
+
+    Rigidbody2D rb;
+    SpriteRenderer sr;
+    public float speed;
+    public Animator animator;
+    public Emotion fishEmotion;
+    private Vector2 target;
+    private Vector2 center = new Vector2(0, 0);
+    private float orbitPosition = 270;
+    public float radius; 
+    public float orbitSpeed;
+    private bool happy = false;
+    private float multiplier;
+    private float distance;
+    private float canvasHeight = 8;
+    private float canvasWidth = 15;
+    public float countdownLength = 0;
+    public float emotionCountdown = 10;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+// https://docs.unity3d.com/ScriptReference/Vector2.MoveTowards.html
+        rb = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
+        fishEmotion = Emotion.neutral;
+        target = new Vector2(Random.value * canvasWidth - canvasWidth / 2, Random.value * canvasHeight - canvasHeight / 2);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        sr.flipX = false;
+        countdownLength -= Time.deltaTime;
+        emotionCountdown -= Time.deltaTime;
+
+        animator.SetBool("IsSad", false);
+        animator.SetBool("IsFear", false);
+        animator.SetBool("IsAnger", false);
+        animator.SetBool("IsHappy", false);
+        animator.SetBool("IsNeutral", false);
+        switch (fishEmotion)
+        {
+            case Emotion.sadness:
+                animator.SetBool("IsSad", true);
+                happy = false;
+                break;
+            case Emotion.fear:
+                animator.SetBool("IsFear", true);
+                happy = false;
+                sr.flipX = false;
+                break;
+            case Emotion.anger:
+                animator.SetBool("IsAnger", true);
+                happy = false;
+                break;
+            case Emotion.joy:
+                animator.SetBool("IsHappy", true);
+                if (!happy)
+                {
+                    happy = true;
+                    orbitPosition = 270;
+                }
+                break;
+            default:
+                animator.SetBool("IsNeutral", true);
+                happy = false;
+                break;
+        }
+
+        if(emotionCountdown < 0)
+        {
+            fishEmotion = Emotion.neutral;
+        }
+
+        rb.velocity = new Vector2(speed*Input.GetAxis("Horizontal"), speed*Input.GetAxis("Vertical"));
+        if(rb.velocity.x > 0.2)
+        {
+            sr.flipX = true;
+        }
+        else if(rb.velocity.x < -0.2)
+        {
+            sr.flipX = false;
+        }
+
+        if(Input.GetAxis("Vertical") < 0.1)
+        {
+            
+            if ((int)(Time.realtimeSinceStartup % 2) == 0)
+            {
+                rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y + 0.2f);
+            }
+            else
+            {
+                rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y - 0.2f);
+            }
+        }
+
+        if(countdownLength < 0)
+        {
+            animator.SetBool("Idling", true);
+            fishEmotion = Emotion.neutral;
+        }
+        else
+        {
+            animator.SetBool("Idling", false);
+
+        }
+
+        if (animator.GetBool("IsHappy"))
+        {
+            if(Vector2.Distance(rb.position, center) < radius*1.1)
+            {
+                Vector2 position = new Vector2(radius * Mathf.Cos(Mathf.PI * orbitPosition / 180), radius * Mathf.Sin(Mathf.PI * orbitPosition / 180));
+                rb.transform.position = position;
+
+                orbitPosition += orbitSpeed;
+                // Someone better versed at maths could probably rewrite this with a fancy sin or cos or something like that
+                if (orbitPosition % 360 < 180)
+                {
+                    sr.flipX = false;
+                }
+                else
+                {
+                    sr.flipX = true;
+                }
+            }
+            else
+            {
+                animator.SetBool("Homing", true);
+            }
+        }
+        animator.SetFloat("Speed", Mathf.Abs(rb.velocity.x));
+
+        if (animator.GetBool("Idling"))
+        {
+            distance = Vector2.Distance(rb.transform.position, target);
+            multiplier = Mathf.Max(distance, 2);
+            float step = speed * Time.deltaTime / 5 * multiplier;
+
+            rb.position = Vector2.MoveTowards(rb.position, target, step);
+            if (distance < 0.25)
+            {
+                target = new Vector2(Random.value * canvasWidth - canvasWidth / 2, Random.value * canvasHeight - canvasHeight / 2);
+            }
+            animator.SetFloat("Speed", 1);
+            if(rb.position.x > target.x)
+            {
+                sr.flipX = false;
+            }
+            else
+            {
+                sr.flipX = true;
+            } 
+        }
+
+        if (!animator.GetBool("Idling") && !animator.GetBool("IsHappy") && Vector2.Distance(rb.transform.position, center) > 0.25 && Mathf.Abs(rb.velocity.x) < 0.5 && Mathf.Abs(rb.velocity.y) < 0.5)
+        {
+            animator.SetBool("Homing", true);
+        }
+
+        if (animator.GetBool("Homing"))
+        {
+            distance = Vector2.Distance(rb.transform.position, center);
+            multiplier = Mathf.Max(distance, 3);
+            float step = speed * Time.deltaTime / 5 * multiplier;
+
+            rb.position = Vector2.MoveTowards(rb.position, center, step);
+            if (distance < 0.1)
+            {
+                animator.SetBool("Homing", false);
+            }
+            animator.SetFloat("Speed", 1);
+            if (rb.position.x > center.x)
+            {
+                sr.flipX = false;
+            }
+            else
+            {
+                sr.flipX = true;
+            }
+        }
+    }
+}
